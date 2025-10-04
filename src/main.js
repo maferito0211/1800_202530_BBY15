280bbece4ea87fb7dc2e1b12e5120e6aafa01f7a
Thhoorr <thorbaker11@gmail.com>
2025-10-04 14:35:13 -0700

    added a welcome popup the disappears when clicked

--- src/main.js
+++ src/main.js
@@ -5,3 +5,8 @@ function sayHello() {
     
 }
 // document.addEventListener('DOMContentLoaded', sayHello);
+
+function disappear() {
+    let popup = document.getElementById("popupcontainer");
+    popup.style.display = "none"; 
+}
