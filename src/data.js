fae4cf3397af83c106227e9b3ee5324d2a99cfbb
Thhoorr <thorbaker11@gmail.com>
2025-10-25 16:07:26 -0700

    Very basic comment section for forum posts

---
+++ src/data.js
@@ -0,0 +1,41 @@
+var threads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Kevin",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Mabel",
+                date: Date.now(),
+                content: "Comment text"
+            },
+            {
+                author: "Mason",
+                date: Date.now(),
+                content: "More comments"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Kevin",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Mabel",
+                date: Date.now(),
+                content: "Comment text"
+            },
+            {
+                author: "Mason",
+                date: Date.now(),
+                content: "More comments"
+            }
+        ]
+    }
+];
+
